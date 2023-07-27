24c47d67f1e619b4d8b8e9f7860200ce5c2dfbae
colonelmaster <113423720+colonelmaster@users.noreply.github.com>
2023-07-27 12:02:20 +0530

    Vanakam Ass

---
+++ LED/tk1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Mini Project")
+root.geometry('480x480')
+customfont = ("Arial",20)
+lbl = Label(root,text = "Welcome to the our program??",font=customfont)
+lbl.pack(side="top")
+root.mainloop()
